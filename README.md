0dd9d8ea90818f5607f65f8e2fc25c0acffbc6bc
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2019-03-26 14:42:16 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# 1.1.1.1 Website
+
+[https://1.1.1.1](https://1.1.1.1)
+
+
+## Getting started
+
+Run yarn locally to install dependencies
+
+```bash
+yarn
+```
+
+
+## Developing locally
+
+```bash
+yarn watch
+```
+
+
+## Building assets
+
+```bash
+yarn build
+```
---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+const { src, dest, parallel, watch } = require("gulp");
+const minifyCSS = require("gulp-csso");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify-es").default;
+
+function css() {
+  return src([
+    // Globals - order could matter
+    "./css/global/box-sizing.css",
+    "./css/global/element-normalization.css",
+    "./css/global/sizes-variables.css",
+    "./css/global/brand-color-variables.css",
+    "./css/global/theme-color-variables.css",
+    "./css/global/selection-color.css",
+    "./css/global/ios-color-fixes.css",
+    "./css/global/font-smoothing.css",
+    "./css/global/html.css",
+    "./css/global/surface.css",
+    "./css/global/body-font-size.css",
+
+
+    // Components - order doesn’t matter but we list for readability
+    "./css/components/copy-link.css",
+    "./css/components/button.css",
+    "./css/components/label-input-wrapper.css",
+    "./css/components/input.css",
+
+    "./css/components/header.css",
+    "./css/components/nav.css",
+    "./css/components/section-copy.css",
+    "./css/components/device-mockup.css",
+
+    "./css/components/one-dot-logo-animated.css",
+    "./css/components/cloudflare-logo.css",
+
+    "./css/components/hero-section.css",
+    "./css/components/app-overview-section.css",
+    "./css/components/performance-section.css",
+    "./css/components/privacy-section.css",
+    "./css/components/warp-section.css"
+  ])
+    .pipe(concat("index.css"))
+    // .pipe(minifyCSS())
+    .pipe(dest("./"));
+}
+
+function js() {
+  return src([
+    "./js/lib/text-balancer.js",
+    "./js/lib/focus-visible-polyfill.js",
+    "./js/lib/basicScroll.js",
+    "./js/index.js"
+  ])
+    .pipe(concat("index.js"))
+    // .pipe(uglify())
+    .pipe(dest("./"));
+}
+
+function watchFiles() {
+  watch(["./css/**/*.css", "js/**/*.js"], parallel(css, js));
+}
+
+exports.js = js;
+exports.css = css;
+exports.watch = watchFiles;
+exports.default = parallel(css, js);
